ee2605e8066ce64fd6db440939bd4d6165a862ca
murakami <murakami@kokochie.jp>
2024-03-19 20:48:32 +0900

    投稿一覧取得/表示

--- blog-with-react-and-firebase/src/App.js
+++ blog-with-react-and-firebase/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import Home from './components/Home';
 import CreatePost from './components/CreatePost';
 import Login from './components/Login';
@@ -15,7 +15,7 @@ function App() {
       <Navbar isAuth={isAuth} />
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/createpost' element={<CreatePost />}></Route>
+        <Route path='/createpost' element={<CreatePost isAuth={isAuth}/>}></Route>
         <Route path='/login' element={<Login setIsAuth={setIsAuth} />}></Route>
         <Route path='/logout' element={<Logout setIsAuth={setIsAuth} />}></Route>
       </Routes>
